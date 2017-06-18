58b4367dd47be9d8b0b568ac1c846d4177ee3747
rio sudarsono <mas.riosudarsono@gmail.com>
2017-06-18 12:57:39 +0700

    update

--- app/src/main/java/nusamandiri/maps3/Fragment/BerandaFragment.java
+++ app/src/main/java/nusamandiri/maps3/Fragment/BerandaFragment.java
@@ -3,13 +3,15 @@ package nusamandiri.maps3.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import nusamandiri.maps3.Activity.MainActivity;
+import com.jaredrummler.materialspinner.MaterialSpinner;
+
 import nusamandiri.maps3.R;
 
 /**
@@ -23,15 +25,35 @@ public class BerandaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View fragmentView = inflater.inflate(R.layout.fr_beranda, container, false);
+
+        MaterialSpinner spinner = (MaterialSpinner) fragmentView.findViewById(R.id.spinner3);
+        spinner.setItems("Pilih Kampus Pertama", "BSI Square", "BSI Cut Meutia", "Universitas Bhayangkara", "Universitas Gunadarma", "STMIK Bani Shaleh", "Stasiun Bekasi");
+        spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
+
+            @Override public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
+                Snackbar.make(view, item + " Telah Dipilih", Snackbar.LENGTH_LONG).show();
+            }
+        });
+        MaterialSpinner spinner1 = (MaterialSpinner) fragmentView.findViewById(R.id.spinner4);
+        spinner1.setItems("Pilih Kampus Kedua", "BSI Square", "BSI Cut Meutia", "Universitas Bhayangkara", "Universitas Gunadarma", "STMIK Bani Shaleh", "Stasiun Bekasi");
+        spinner1.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
+
+            @Override public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
+                Snackbar.make(view, item + " Telah Dipilih", Snackbar.LENGTH_LONG).show();
+            }
+        });
+
         Button btcarirute = (Button) fragmentView.findViewById(R.id.btn_carirute);
         btcarirute.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Intent MainActivity
-                        = new Intent(getActivity(),MainActivity.class);
+                        = new Intent(getActivity(), nusamandiri.maps3.Activity.MainActivity.class);
                 startActivity(MainActivity);
             }
         });
+
         return fragmentView;
     }
 }
