140d57ae9dcf5b72f684946cb88392f16804707d
rio sudarsono <mas.riosudarsono@gmail.com>
2017-07-21 16:22:13 +0700

    Sukses

---
+++ app/src/main/java/nusamandiri/maps3/Activity/Square_Meutia.java
@@ -0,0 +1,15 @@
+package nusamandiri.maps3.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nusamandiri.maps3.R;
+
+public class Square_Meutia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_square__meutia);
+    }
+}
---
+++ app/src/main/java/nusamandiri/maps3/Activity/Terminal_Unisma.java
@@ -0,0 +1,15 @@
+package nusamandiri.maps3.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nusamandiri.maps3.R;
+
+public class Terminal_Unisma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terminal__unisma);
+    }
+}
