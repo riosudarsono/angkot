7fe23c7d29ce983d9151875afa5e019a8e4f0860
rio sudarsono <mas.riosudarsono@gmail.com>
2017-07-26 15:14:04 +0700

    Sukses

--- app/src/main/java/nusamandiri/maps3/Activity/DetailKampus/Universitas_Gunadarma.java
+++ app/src/main/java/nusamandiri/maps3/Activity/DetailKampus/Universitas_Gunadarma.java
@@ -1,7 +1,10 @@
 package nusamandiri.maps3.Activity.DetailKampus;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.Spanned;
+import android.webkit.WebView;
 
 import nusamandiri.maps3.R;
 
@@ -11,5 +14,13 @@ public class Universitas_Gunadarma extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_universitas__gunadarma);
+
+
+        String htmlAsString = getString(R.string.gundar);      // used by WebView
+        Spanned htmlAsSpanned = Html.fromHtml(htmlAsString); // used by TextView
+
+        WebView webView = (WebView) findViewById(R.id.visi_gundar);
+        webView.loadDataWithBaseURL(null, htmlAsString, "text/html", "utf-8", null);
+
     }
 }
