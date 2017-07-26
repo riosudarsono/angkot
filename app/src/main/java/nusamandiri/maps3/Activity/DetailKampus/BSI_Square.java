985522a7ba5feb0a360107e404f71b97b3cb65ef
rio sudarsono <mas.riosudarsono@gmail.com>
2017-07-26 17:22:37 +0700

    Sukses

--- app/src/main/java/nusamandiri/maps3/Activity/DetailKampus/BSI_Square.java
+++ app/src/main/java/nusamandiri/maps3/Activity/DetailKampus/BSI_Square.java
@@ -2,6 +2,9 @@ package nusamandiri.maps3.Activity.DetailKampus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.webkit.WebView;
 
 import nusamandiri.maps3.R;
 
@@ -11,5 +14,11 @@ public class BSI_Square extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bsi__square);
+
+        String htmlAsString = getString(R.string.square);      // used by WebView
+        Spanned htmlAsSpanned = Html.fromHtml(htmlAsString); // used by TextView
+
+        WebView webView = (WebView) findViewById(R.id.visi_square);
+        webView.loadDataWithBaseURL(null, htmlAsString, "text/html", "utf-8", null);
     }
 }
--- app/src/main/java/nusamandiri/maps3/Activity/DetailKampus/Universitas_Bhayangkara.java
+++ app/src/main/java/nusamandiri/maps3/Activity/DetailKampus/Universitas_Bhayangkara.java
@@ -2,6 +2,9 @@ package nusamandiri.maps3.Activity.DetailKampus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.webkit.WebView;
 
 import nusamandiri.maps3.R;
 
@@ -11,5 +14,12 @@ public class Universitas_Bhayangkara extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_universitas__bhayangkara);
+
+        String htmlAsString = getString(R.string.ubhara);      // used by WebView
+        Spanned htmlAsSpanned = Html.fromHtml(htmlAsString); // used by TextView
+
+        WebView webView = (WebView) findViewById(R.id.visi_ubhara);
+        webView.loadDataWithBaseURL(null, htmlAsString, "text/html", "utf-8", null);
+
     }
 }
