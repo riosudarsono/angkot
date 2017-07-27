e046fa14d6ba5a3da15408fd727244ab42e3678d
rio sudarsono <mas.riosudarsono@gmail.com>
2017-07-27 07:25:25 +0700

    Sukses

--- app/src/main/java/nusamandiri/maps3/Activity/DetailKampus/BSI_CutMeutia.java
+++ app/src/main/java/nusamandiri/maps3/Activity/DetailKampus/BSI_CutMeutia.java
@@ -2,6 +2,9 @@ package nusamandiri.maps3.Activity.DetailKampus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.webkit.WebView;
 
 import nusamandiri.maps3.R;
 
@@ -11,5 +14,12 @@ public class BSI_CutMeutia extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bsi__cut_meutia);
+
+        String htmlAsString = getString(R.string.meutia);      // used by WebView
+        Spanned htmlAsSpanned = Html.fromHtml(htmlAsString); // used by TextView
+
+        WebView webView = (WebView) findViewById(R.id.visi_meutia);
+        webView.loadDataWithBaseURL(null, htmlAsString, "text/html", "utf-8", null);
+
     }
 }
