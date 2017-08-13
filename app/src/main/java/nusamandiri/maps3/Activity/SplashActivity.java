5304e13bf0939a8b31d58c6c48c64955512062c9
rio sudarsono <mas.riosudarsono@gmail.com>
2017-08-13 11:06:36 +0700

    Sukses

---
+++ app/src/main/java/nusamandiri/maps3/Activity/SplashActivity.java
@@ -0,0 +1,47 @@
+package nusamandiri.maps3.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import nusamandiri.maps3.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int splashInterval = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(SplashActivity.this, BerandaActivity.class);
+                startActivity(i); // menghubungkan activity splashscren ke main activity dengan intent
+
+
+                //jeda selesai Splashscreen
+                this.finish();
+            }
+
+            private void finish() {
+                // TODO Auto-generated method stub
+
+            }
+        }, splashInterval);
+
+    };
+
+}
